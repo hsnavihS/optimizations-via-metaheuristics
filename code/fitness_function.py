9e94b412a5c11abf2407652d32dd45fbdea9f54f
Ankesh Pandey <77636343+gibsonjackson@users.noreply.github.com>
2023-04-24 08:12:39 +0530

    Fitness Function Modified

--- code/fitness_function.py
+++ code/fitness_function.py
@@ -1,13 +1,12 @@
-
 from basic_face_recognition import fr
-
-
+ 
+ 
 def fitness_function(scale_factor):
     '''
     Universal fitness function for all search algorithms.
     If there is a label mismatch, return zero.
     Otherwise, return the confidence value.
     '''
-
+ 
     person_name, confidence = fr(scale_factor)
     return confidence, person_name
